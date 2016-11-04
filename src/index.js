fd859e9c66a6c80eeb5ab7189c6cf0617386e311
rot1024 <rot1024@gmail.com>
2016-11-04 18:33:48 +0900

    Fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 
 import Viewer from "cesium/Source/Widgets/Viewer/Viewer";
 
+// eslint-disable-next-line no-unused-vars
 const viewer = new Viewer(document.getElementById("cesium"));
 
 if (module && module.hot) {
