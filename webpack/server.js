97437eacbde175a51a8ecd0698f5bfd8f65095d4
rot1024 <rot1024@gmail.com>
2016-11-04 16:37:24 +0900

    Add open option to dev sever

--- webpack/server.js
+++ webpack/server.js
@@ -5,6 +5,8 @@ const WebpackDevServer = require("webpack-dev-server");
 const config = require("./config")();
 const open = require("open");
 
+const argv = process.argv.slice(2);
+
 new WebpackDevServer(webpack(config), {
   hot: true,
   publicPath: config.output.publicPath,
@@ -12,7 +14,10 @@ new WebpackDevServer(webpack(config), {
 }).listen(config.devServerPort, "localhost", err => {
   if (err) {
     console.log(err);
+    return;
   }
   console.info(`=> Listening on http://localhost:${config.devServerPort}`);
-  open(`http://localhost:${config.devServerPort}/`);
+  if (argv.includes("-o") || argv.includes("--open")) {
+    open(`http://localhost:${config.devServerPort}/`);
+  }
 });
