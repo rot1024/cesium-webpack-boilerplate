01477b9546dc1a115ea12483fd12a06e1fc3b698
rot1024 <aayhrot@gmail.com>
2018-03-26 03:56:00 +0900

    refactor

--- src/bootstrap.js
+++
@@ -1,4 +0,0 @@
-import "cesium/Source/Widgets/widgets.css";
-import buildModuleUrl from "cesium/Source/Core/buildModuleUrl";
-
-buildModuleUrl.setBaseUrl("./cesium/");
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import Viewer from "cesium/Source/Widgets/Viewer/Viewer";
+import Viewer from "cesium/Widgets/Viewer/Viewer";
 
-import "./bootstrap";
+import "cesium/Widgets/widgets.css";
 import "./style.css";
 
 // eslint-disable-next-line no-unused-vars
 const viewer = new Viewer(document.getElementById("cesium"));
-
-if (module.hot) {
-  module.hot.accept();
-}
